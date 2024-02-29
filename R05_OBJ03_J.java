3a22f06c6eb952ede0934ca53c8dbdf1565a3d5b
litzyventura <102265793+litzyventura@users.noreply.github.com>
2024-02-29 12:46:33 -0500

    R05_OBJ03_J.java

--- R05_OBJ03_J.java
+++ R05_OBJ03_J.java
@@ -1,11 +1,11 @@
 class ListUtility {
-  private static void addToList(List<String> list, String str) {
-    list.add(str);     // No warning generated
+  private static void addToList(List list, Object obj) {
+    list.add(obj); // Unchecked warning
   }
  
   public static void main(String[] args) {
     List<String> list = new ArrayList<String> ();
-    addToList(list, "42");
-    System.out.println(list.get(0));
+    addToList(list, 42);
+    System.out.println(list.get(0));  // Throws ClassCastException
   }
 }
